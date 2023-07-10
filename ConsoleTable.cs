093601aefb8573c8aa5355c603ea02da782fccd6
Remzi85 <ramzila@code.edu.az>
2023-07-10 23:21:01 +0400

    Add project files.

---
+++ ConsoleTable.cs
@@ -0,0 +1,21 @@
+﻿namespace Marketing_Console
+{
+    internal class ConsoleTable
+    {
+        private string v1;
+        private string v2;
+        private string v3;
+
+        public ConsoleTable(string v1, string v2, string v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+        internal void AddRow(int v1, string v2, string v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Marketing_Console
+{
+    public class Program
+    {
+        public static void Main(string[] args) { 
+        
+                int option;
+
+                do
+                {
+                    Console.WriteLine("1.Products ");
+                    Console.WriteLine("2. Add Product");
+                    Console.WriteLine("3. Delete Product ");
+                    Console.WriteLine("4. Sales");
+                    Console.WriteLine("5. Add Sale ");
+                    Console.WriteLine("6. Delete Sale");
+                    Console.WriteLine("7. Sale Items");
+                    Console.WriteLine("8. Add Sale Item");
+                    Console.WriteLine("9. Delete Sale Item ");
+                    Console.WriteLine("10. Report");
+                    Console.WriteLine("0. Exit");
+                    Console.WriteLine("-----------");
+
+                    Console.WriteLine("Enter option:");
+
+                    while (!int.TryParse(Console.ReadLine(), out option))
+                    {
+                        Console.WriteLine("Invalid number!");
+                        Console.WriteLine("-----------");
+                        Console.WriteLine("Enter option:");
+                    }
+
+                    switch (option)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        case 9:
+                            break;
+                        case 10:
+                            break;
+                        case 0:
+                            Console.WriteLine("Bye!");
+                            break;
+                        default:
+                            Console.WriteLine("No such option!");
+                            break;
+                    }
+
+                } while (option != 0);
+            }
+        }
+    }
